9285c6427e1ac0f7f244df63daea3f19f03a4678
Kien Tran <kientrungtran.dev@gmail.com>
2021-10-19 20:45:55 +0700

    [docker-compose] update

--- services/license-service/src/main/kotlin/com/kien/licensing/config/R2dbcConfig.kt
+++ services/license-service/src/main/kotlin/com/kien/licensing/config/R2dbcConfig.kt
@@ -4,12 +4,10 @@ import io.r2dbc.spi.ConnectionFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ClassPathResource
-import org.springframework.data.web.config.EnableSpringDataWebSupport
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
 
 @Configuration
-@EnableSpringDataWebSupport
 class R2dbcConfig {
 
     @Bean
