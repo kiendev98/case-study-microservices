537b510845a3955b533250b6787c4f9a71eb8855
Kien Tran <kientrungtran.dev@gmail.com>
2021-10-22 12:10:00 +0700

    [license-service] fist step of circuit breakder

--- services/license-service/src/main/kotlin/com/kien/licensing/config/WebConfig.kt
+++ services/license-service/src/main/kotlin/com/kien/licensing/config/WebConfig.kt
@@ -1,9 +1,15 @@
 package com.kien.licensing.config
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
+import io.github.resilience4j.timelimiter.TimeLimiterConfig
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder
+import org.springframework.cloud.client.circuitbreaker.Customizer
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.client.WebClient
+import java.time.Duration
 
 
 @Configuration
@@ -12,4 +18,16 @@ class WebConfig {
     @LoadBalanced
     @Bean
     fun webClientBuilder(): WebClient.Builder = WebClient.builder()
+
+    @Bean
+    fun defaultCustomizer(): Customizer<ReactiveResilience4JCircuitBreakerFactory> {
+        return Customizer<ReactiveResilience4JCircuitBreakerFactory> { factory ->
+            factory.configureDefault { id ->
+                Resilience4JConfigBuilder(id)
+                    .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                    .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+                    .build()
+            }
+        }
+    }
 }
--- services/license-service/src/main/kotlin/com/kien/licensing/service/LicenseService.kt
+++ services/license-service/src/main/kotlin/com/kien/licensing/service/LicenseService.kt
@@ -3,21 +3,42 @@ package com.kien.licensing.service
 import com.kien.licensing.model.License
 import com.kien.licensing.repository.LicenseRepository
 import com.kien.licensing.service.client.OrganizationDiscoveryClient
-import org.springframework.data.annotation.Id
-import org.springframework.data.relational.core.mapping.Column
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
+import java.util.concurrent.TimeoutException
+import kotlin.random.Random
 
 @Service
 class LicenseService(
     private val licenseRepository: LicenseRepository,
     private val organizationDiscoveryClient: OrganizationDiscoveryClient
 ) {
+
+    private fun randomRunLong() {
+        val randomNum = Random.nextInt(3) + 1
+        sleep()
+    }
+
+    private fun sleep() {
+        try {
+            Thread.sleep(5000)
+            throw TimeoutException()
+        } catch (e: InterruptedException) {
+            println(e.message)
+        }
+    }
+
+    @CircuitBreaker(name = "licenseService")
     fun getLicense(licenseId: Long, organizationId: Long): Mono<License> =
         Mono.zip(
             licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId),
             organizationDiscoveryClient.getOrganization(organizationId)
-        ).map { it.t1 + it.t2 }
+        )
+            .map {
+                randomRunLong()
+                it.t1 + it.t2
+            }
 
     fun updateLicense(license: License): Mono<License> = licenseRepository.save(license)
 
